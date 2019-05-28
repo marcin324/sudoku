51d01fc73c215365e000b342dc80b16383833b5f
Marcin Olszowski <olszyna38@gmail.com>
2019-05-28 14:50:53 +0200

    Updates

---
+++ precache-manifest.c05b167e3f8406cee3483f4e5eb24162.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5b669627b2f867a2b56dfd351a8a70bd",
+    "url": "/sudoku/index.html"
+  },
+  {
+    "revision": "ca70ee2de4f22bdffe9d",
+    "url": "/sudoku/static/css/main.3c1f05dd.chunk.css"
+  },
+  {
+    "revision": "e8bfa6d65cebddec4d48",
+    "url": "/sudoku/static/js/2.e95650df.chunk.js"
+  },
+  {
+    "revision": "ca70ee2de4f22bdffe9d",
+    "url": "/sudoku/static/js/main.b549d3c5.chunk.js"
+  },
+  {
+    "revision": "2376657f656b447c3d4b",
+    "url": "/sudoku/static/js/runtime~main.e7756cf6.js"
+  }
+]);
